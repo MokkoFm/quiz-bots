f001cca1aa77a2be4b5a32de940ee1af28ee7341
MokkoFm <mokkofmpoetry@gmail.com>
2021-03-04 22:42:28 +0200

    add functions in code

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
+def get_quiz():
+    filename = 'quiz-questions/1vs1200.txt'
+    with open(filename, 'r', encoding='KOI8-R') as file:
+        questions = file.read()
 
-filename = 'quiz-questions/1vs1200.txt'
-with open(filename, 'r', encoding='KOI8-R') as file:
-    questions = file.read()
+    paragraphs = questions.split('Вопрос')[3::]
+    questions = []
+    answers = []
+    for paragraph in paragraphs:
+        try:
+            question = paragraph.split('Вопрос')[0].split('Ответ')[0].split(':\n')[1]
+            questions.append(question)
+            answer = paragraph.split('Ответ')[1].split(':')[1].split('\n')[1]
+            answers.append(answer)
+        except IndexError:
+            continue
 
-paragraphs = questions.split('Вопрос')[3::]
-questions = []
-answers = []
-for paragraph in paragraphs:
-    try:
-        question = paragraph.split('Вопрос')[0].split('Ответ')[0].split(':\n')[1]
-        questions.append(question)
-        answer = paragraph.split('Ответ')[1].split(':')[1].split('\n')[1]
-        answers.append(answer)
-    except IndexError:
-        continue
+    quiz = dict(zip(questions, answers))
+    return quiz
 
-quiz = dict(zip(questions, answers))
+
+def main():
+    get_quiz()
+
+
+if __name__ == "__main__":
+    main()
