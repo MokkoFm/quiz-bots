a16c9168f9eed293cb9495e3c65be40ad48849b0
MokkoFm <mokkofmpoetry@gmail.com>
2021-03-04 22:39:11 +0200

    get a dict with questions and answers

---
+++ main.py
@@ -0,0 +1,18 @@
+
+filename = 'quiz-questions/1vs1200.txt'
+with open(filename, 'r', encoding='KOI8-R') as file:
+    questions = file.read()
+
+paragraphs = questions.split('Вопрос')[3::]
+questions = []
+answers = []
+for paragraph in paragraphs:
+    try:
+        question = paragraph.split('Вопрос')[0].split('Ответ')[0].split(':\n')[1]
+        questions.append(question)
+        answer = paragraph.split('Ответ')[1].split(':')[1].split('\n')[1]
+        answers.append(answer)
+    except IndexError:
+        continue
+
+quiz = dict(zip(questions, answers))
